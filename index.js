dc48a2ab9b7c61b59fb8ccd90622c5f8260a33a0
kauaferreirac <127845047+kauaferreirac@users.noreply.github.com>
2024-09-17 01:16:13 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const { log } = require("console");
+
+const primeiroNome = "Kauã";
+const sobrenome = "Ferreira";
+const idade = 16;
+const cidadeNatal = "São José dos Pinhais"
+const altura = 1.75;
+
+console.log("O meu primeiro nome é:");
+console.log(primeiroNome);
+console.log("o meu sobrenome é:");
+console.log(sobrenome);
+console.log("a minha idade é:");
+console.log(idade);
+console.log("eu nasci na cidade de:");
+console.log(cidadeNatal);
+console.log("a minha altura é:");
+console.log(altura);
